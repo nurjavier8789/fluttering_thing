1a9ad3958e222e66413fbfb5a74ae7ad3176252d
Nur Javier Prasetyo <nurjavierprasetyo8789@gmail.com>
2023-10-13 13:13:12 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# This is flutter test
+
+I just want to make my own apps 😅
+
+feel free to review my code :D
---
+++ android/app/src/main/java/io/github/nurjavier8789/fluttering_thing/MainActivity.java
@@ -0,0 +1,6 @@
+package io.github.nurjavier8789.fluttering_thing;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
